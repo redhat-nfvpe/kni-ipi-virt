ed7488f5b0975ade9fce76a97c1ce662a8fbb13b
Andrew Bays <andrew.bays@gmail.com>
2020-04-21 15:08:41 -0400

    Dnsmasq hostfile MAC override option

--- dhcp/start.sh
+++ dhcp/start.sh
@@ -17,13 +17,30 @@ envsubst < "$PROJECT_DIR/dhcp/dnsmasq.conf.tmpl" > "${OUTPUT_DIR}"/bm/etc/dnsmas
 BM_MAC="$(ip l show "$BM_INTF" | grep "link/ether" | awk {'print $2'})"
 DNSMASQ_HOSTS="$BM_MAC,$BM_GW_IP,provisioner.$CLUSTER_NAME.$CLUSTER_DOMAIN"
 
-for i in $(seq 0 $((NUM_MASTERS - 1))); do
-    DNSMASQ_HOSTS="$DNSMASQ_HOSTS\n$MASTER_BM_MAC_PREFIX$i,10.0.1.12$i,$CLUSTER_NAME-master-$i.$CLUSTER_NAME.$CLUSTER_DOMAIN"
-done
+if [[ -z "$DHCP_BM_MACS" ]]; then
+    for i in $(seq 0 $((NUM_MASTERS - 1))); do
+        DNSMASQ_HOSTS="$DNSMASQ_HOSTS\n$MASTER_BM_MAC_PREFIX$i,10.0.1.12$i,$CLUSTER_NAME-master-$i.$CLUSTER_NAME.$CLUSTER_DOMAIN"
+    done
 
-for i in $(seq 0 $((NUM_WORKERS - 1))); do
-    DNSMASQ_HOSTS="$DNSMASQ_HOSTS\n$WORKER_BM_MAC_PREFIX$i,10.0.1.13$i,$CLUSTER_NAME-worker-$i.$CLUSTER_NAME.$CLUSTER_DOMAIN"
-done
+    for i in $(seq 0 $((NUM_WORKERS - 1))); do
+        DNSMASQ_HOSTS="$DNSMASQ_HOSTS\n$WORKER_BM_MAC_PREFIX$i,10.0.1.13$i,$CLUSTER_NAME-worker-$i.$CLUSTER_NAME.$CLUSTER_DOMAIN"
+    done
+else
+    IFS=', ' read -r -a BM_MACS <<< "$DHCP_BM_MACS"
+
+    if [[ ${#BM_MACS[@]} != $((NUM_MASTERS + NUM_WORKERS)) ]]; then
+        echo "Dnsmasq container config generation: Invalid DHCP_BM_MACS count!  ${#BM_MACS[@]} != $((NUM_MASTERS + NUM_WORKERS))"
+        exit 1
+    fi
+
+    for i in $(seq 0 $((NUM_MASTERS - 1))); do
+        DNSMASQ_HOSTS="$DNSMASQ_HOSTS\n${BM_MACS[$i]},10.0.1.12$i,$CLUSTER_NAME-master-$i.$CLUSTER_NAME.$CLUSTER_DOMAIN"
+    done
+
+    for i in $(seq 0 $((NUM_WORKERS - 1))); do
+        DNSMASQ_HOSTS="$DNSMASQ_HOSTS\n${BM_MACS[$((i + NUM_MASTERS))]},10.0.1.13$i,$CLUSTER_NAME-worker-$i.$CLUSTER_NAME.$CLUSTER_DOMAIN"
+    done
+fi
 
 echo -e "$DNSMASQ_HOSTS" > "${OUTPUT_DIR}"/bm/etc/dnsmasq.d/dnsmasq.hostsfile
 
